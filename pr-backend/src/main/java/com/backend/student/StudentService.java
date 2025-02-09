4c9ce617d0dc5bedc68ab731d3bada519f6f10df
Adelina Vaitus <adelinavaitus@gmail.com>
2025-02-09 11:57:15 +0200

    Added comments for classes related to the Student entity

--- pr-backend/src/main/java/com/backend/student/StudentService.java
+++ pr-backend/src/main/java/com/backend/student/StudentService.java
@@ -25,6 +25,7 @@ public class StudentService {
     @Autowired
     private UserRepository userRepository;
 
+    // Retrieves all students and maps them into a list of FormGetStudent objects
     public List<FormGetStudent> getAllFormGetStudents(){
         List<FormGetStudent> formGetStudents = new ArrayList<>();
         List<Student>  students = studentRepository.findAll();
@@ -43,6 +44,7 @@ public class StudentService {
         return formGetStudents;
     }
 
+    // Retrieves all students by faculty ID and maps them into a list of FormGetStudent objects
     public List<FormGetStudent> getAllFormGetStudentsByFacultyId(int facultyId){
         List<FormGetStudent> formGetStudents = new ArrayList<>();
         List<Student>  students = studentRepository.findByFacultyId(facultyId);
@@ -61,46 +63,56 @@ public class StudentService {
         return formGetStudents;
     }
 
+    // Retrieves students by faculty ID
     public List<Student> getStudentsByFacultyId(int facultyId){
         List<Student> students = studentRepository.findByFacultyId(facultyId);
         return students;
     }
 
+    // Retrieves all students from the repository
     public List<Student> getAllStudents(){
         List<Student> students = studentRepository.findAll();
         return students;
     }
+
+    // Retrieves a student by their ID
     public Student getStudentById(int id){
         Student student = studentRepository.findById(id).get();
         return  student;
     }
 
+    // Inserts a new student into the repository
     public Student insertStudent(Student student){
         return  studentRepository.save(student);
     }
 
+    // Updates the details of an existing student
     public Student updateStudent (Student student){
         return studentRepository.save(student);
     }
 
+    // Updates the first name of a student based on their ID
     public Student updateFirstNameByStudentId(int id, String firstName){
         Student student = getStudentById(id);
         student.setFirstName(firstName);
         return studentRepository.save(student);
     }
 
+    // Updates the last name of a student based on their ID
     public Student updateLastNameByStudentId(int id, String lastName){
         Student student = getStudentById(id);
         student.setLastName(lastName);
         return studentRepository.save(student);
     }
 
+    // Updates the group of a student based on their ID
     public Student updateGroupByStudentId(int id, String group){
         Student student = getStudentById(id);
         student.setGroup(group);
         return studentRepository.save(student);
     }
 
+    // Updates the faculty of a student based on their ID
     public Student updateFacultyByStudentId(int id, String facultyId){
         int fid = Integer.valueOf(facultyId);
         Faculty faculty = facultyService.getFacultyById(fid);
@@ -109,14 +121,14 @@ public class StudentService {
         return studentRepository.save(student);
     }
 
-
+    // Deletes a student from the repository
     public Student deleteStudent(Student student){
         Student oldStudent = student;
         studentRepository.delete(student);
         return oldStudent;
     }
 
-
+    // Checks if a phone number is already in use by a student
     public boolean isPhoneNoAlreadyIsUse(String phoneNo){
         Boolean response = studentRepository.existsStudentByPhoneNo(phoneNo);
         return response;
