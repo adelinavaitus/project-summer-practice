1d4fdc694c3e7887f5596e2d2c21a8a9bd593840
Adelina Vaitus <adelinavaitus@gmail.com>
2025-02-09 18:06:09 +0200

    Added comments for classes related to the Faculty entity

--- pr-backend/src/main/java/com/backend/faculty/FacultyController.java
+++ pr-backend/src/main/java/com/backend/faculty/FacultyController.java
@@ -16,42 +16,59 @@ public class FacultyController {
     @Autowired
     private FacultyService facultyService;
 
-
+    // Endpoint to get all faculties
     @GetMapping()
     public ResponseEntity<List<Faculty>> getFaculties(){
-        ResponseEntity responseEntity = ResponseEntity.status(HttpStatus.OK).body(facultyService.getAllFaculties());
+        ResponseEntity responseEntity = ResponseEntity.status(HttpStatus.OK)
+                .body(facultyService.getAllFaculties());
+
         return responseEntity;
     }
 
+    // Endpoint to get a specific faculty by its ID
     @GetMapping("/{id}")
     public ResponseEntity<Faculty> getFacultyById(@PathVariable Integer id){
-        ResponseEntity responseEntity = ResponseEntity.status(HttpStatus.OK).body(facultyService.getFacultyById(id));
+        ResponseEntity responseEntity = ResponseEntity.status(HttpStatus.OK)
+                .body(facultyService.getFacultyById(id));
+
         return responseEntity;
     }
 
+    // Endpoint to get a specific faculty by its name
     @GetMapping("/{name}")
     public ResponseEntity<Faculty> getFacultyByName(@PathVariable String name){
-        ResponseEntity responseEntity = ResponseEntity.status(HttpStatus.OK).body(facultyService.getFacultyByName(name));
+        ResponseEntity responseEntity = ResponseEntity.status(HttpStatus.OK)
+                .body(facultyService.getFacultyByName(name));
+
         return responseEntity;
     }
+
+    // Endpoint to insert a new faculty
     @PostMapping()
     public ResponseEntity<Faculty> insertFaculty(@RequestBody Faculty faculty){
-        ResponseEntity responseEntity = ResponseEntity.status(HttpStatus.OK).body(facultyService.insertFaculty(faculty));
+        ResponseEntity responseEntity = ResponseEntity.status(HttpStatus.OK)
+                .body(facultyService.insertFaculty(faculty));
+
         return responseEntity;
     }
 
+    // Endpoint to update an existing faculty
     @PutMapping()
     public ResponseEntity<Faculty> updateFaculty(@RequestBody Faculty faculty){
-        ResponseEntity responseEntity = ResponseEntity.status(HttpStatus.OK).body(facultyService.updateFaculty(faculty));
+        ResponseEntity responseEntity = ResponseEntity.status(HttpStatus.OK)
+                .body(facultyService.updateFaculty(faculty));
+
         return responseEntity;
     }
 
+    // Endpoint to update the description of a faculty by its ID
     @PutMapping("/{id}/description")
-    public ResponseEntity<Faculty> updateDescriptionById(@PathVariable Integer id, @RequestBody String description){
+    public ResponseEntity<Faculty> updateDescriptionById(@PathVariable Integer id,
+                                                         @RequestBody String description){
         description = description.substring(16, description.length()-2);
-        ResponseEntity responseEntity = ResponseEntity.status(HttpStatus.OK).body(facultyService.updateDescriptionById(id, description));
+        ResponseEntity responseEntity = ResponseEntity.status(HttpStatus.OK)
+                .body(facultyService.updateDescriptionById(id, description));
+
         return responseEntity;
     }
-
-
 }
--- pr-backend/src/main/java/com/backend/faculty/FacultyService.java
+++ pr-backend/src/main/java/com/backend/faculty/FacultyService.java
@@ -11,41 +11,54 @@ public class FacultyService {
     @Autowired
     private FacultyRepository facultyRepository;
 
+    // Retrieve all faculties from the repository
     public List<Faculty> getAllFaculties(){
         List<Faculty> faculties = facultyRepository.findAll();
+
         return faculties;
     }
 
+    // Retrieve a specific faculty by its ID
     public Faculty getFacultyById(int id){
         Faculty faculty = facultyRepository.findById(id).get();
+
         return faculty;
     }
 
+    // Retrieve a specific faculty by its name
     public Faculty getFacultyByName(String name){
         Faculty faculty = facultyRepository.getByName(name);
+
         return faculty;
     }
 
+    // Insert a new faculty into the repository
     public Faculty insertFaculty(Faculty faculty){
         Faculty newFaculty = facultyRepository.save(faculty);
+
         return newFaculty;
     }
 
+    // Update an existing faculty
     public Faculty updateFaculty (Faculty faculty){
         Faculty updatedFaculty = facultyRepository.save(faculty);
+
         return updatedFaculty;
     }
 
+    // Update the description of a faculty by its ID
     public Faculty updateDescriptionById (int id, String description){
         Faculty faculty = getFacultyById(id);
         faculty.setDescription(description);
+
         return facultyRepository.save(faculty);
     }
 
+    // Delete a faculty from the repository
     public  Faculty deleteFaculty(Faculty faculty){
         Faculty oldFaculty = faculty;
         facultyRepository.delete(faculty);
+
         return oldFaculty;
     }
-
 }
