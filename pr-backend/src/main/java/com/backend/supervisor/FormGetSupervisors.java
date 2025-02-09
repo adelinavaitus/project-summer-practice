c90534e161460e9471d82b1313c0f1fe482534f8
Adelina Vaitus <adelinavaitus@gmail.com>
2025-02-09 11:34:42 +0200

    Added comments for classes related to the Supervisor entity

--- pr-backend/src/main/java/com/backend/supervisor/FormGetSupervisors.java
+++ pr-backend/src/main/java/com/backend/supervisor/FormGetSupervisors.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+// This class is a DTO used for holding data about a Supervisor in the context of a form
 public class FormGetSupervisors {
     private int id;
     private String firstName;
--- pr-backend/src/main/java/com/backend/supervisor/SupervisorRepository.java
+++ pr-backend/src/main/java/com/backend/supervisor/SupervisorRepository.java
@@ -1,6 +1,5 @@
 package com.backend.supervisor;
 
-import net.bytebuddy.implementation.bind.annotation.Super;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
