4f2d47bf0e1950a8a12e8d7ac1a255504b15627f
Adelina Vaitus <adelinavaitus@gmail.com>
2025-02-09 12:01:03 +0200

    Added comments for classes related to the Role entity

--- pr-backend/src/main/java/com/backend/role/RoleService.java
+++ pr-backend/src/main/java/com/backend/role/RoleService.java
@@ -11,18 +11,22 @@ public class RoleService {
     @Autowired
     private RoleRepository roleRepository;
 
+    // Method to retrieve all roles from the database
     public List<Role> getRoles() {
         return roleRepository.findAll();
     }
 
+    // Method to insert a new role into the database
     public Role insertRole(Role role) {
         return roleRepository.save(role);
     }
 
+    // Method to update an existing role in the database
     public Role updateRole(Role role) {
         return roleRepository.save(role);
     }
 
+    // Method to delete a role from the database
     public Role deleteRole(Role role) {
         Role oldRole = role;
         roleRepository.delete(role);
