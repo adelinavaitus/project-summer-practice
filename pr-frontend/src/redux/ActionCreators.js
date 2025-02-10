b341fa9771a63664ff73d8e8350edf2ffb383ad0
Adelina Vaitus <adelinavaitus@gmail.com>
2025-02-10 16:17:14 +0200

    Added comments for redux files

--- pr-frontend/src/redux/ActionCreators.js
+++ pr-frontend/src/redux/ActionCreators.js
@@ -1,128 +1,125 @@
-import * as ActionTypes from './ActionTypes';
-import axios from 'axios';
+import * as ActionTypes from './ActionTypes'; // Import action types
+import axios from 'axios';  // Import axios for making HTTP requests
 
+// Action to handle login form submission
 export const postFormLogin = (data) => (dispatch) => {
   axios
-    .post("/login", data)
+    .post("/login", data) // Send POST request to login endpoint with the provided data
     .then((res) => {
-      localStorage.setItem("token", res.data.accessToken);
-      return res.data;
+      localStorage.setItem("token", res.data.accessToken);   // Save the received token in localStorage
+      return res.data;  // Return the response data
     })
     .then((result) =>
       dispatch({
-        type: ActionTypes.POST_LOGIN_SUCCESS,
+        type: ActionTypes.POST_LOGIN_SUCCESS,  // Dispatch success action with the result
         payload: result,
       })
     )
     .catch((err) =>
       dispatch({
-        type: ActionTypes.POST_LOGIN_FAILURE,
+        type: ActionTypes.POST_LOGIN_FAILURE, // Dispatch failure action if there's an error
         payload: err.code,
       })
     )
-    
 };
 
+// Action to handle company registration form submission
 export const postFormRegisterCompany = (data) => (dispatch) => {
   axios
-    .post(`/register-company`, data)
+    .post(`/register-company`, data)   // Send POST request to register company endpoint
     .then((res) => {
-      return res.data;
+      return res.data;  // Return the response data
     })
     .then((result) =>
       dispatch({
-        type: ActionTypes.POST_REGISTER_SUCCESS,
+        type: ActionTypes.POST_REGISTER_SUCCESS,  // Dispatch success action with the result
         payload: result,
       })
     )
     .then(() => {
-      window.location.href= "/login"
+      window.location.href= "/login"   // Redirect to login page after successful registration
     })
     .catch((err) => {
       dispatch({
-        type: ActionTypes.POST_REGISTER_FAILURE,
+        type: ActionTypes.POST_REGISTER_FAILURE,   // Dispatch failure action if there's an error
         payload: err.response.data.message,
       })
-
     });
-
-
-
 };
 
+// Action to handle student registration form submission
 export const postFormRegisterStudent = (data) => (dispatch) => {
   axios
-    .post(`/register-student`, data)
+    .post(`/register-student`, data)   // Send POST request to register student endpoint
     .then((res) => {
-      return res.data;
+      return res.data;  // Return the response data
     })
     .then((result) =>
       dispatch({
-        type: ActionTypes.POST_REGISTER_STUDENT_SUCCESS,
+        type: ActionTypes.POST_REGISTER_STUDENT_SUCCESS,   // Dispatch success action with the result
         payload: result,
       })
     )
     .then(() => {
-      window.location.href= "/login"
+      window.location.href= "/login"   // Redirect to login page after successful registration
     })
     .catch((err) => {
       dispatch({
-        type: ActionTypes.POST_REGISTER_STUDENT_FAILURE,
+        type: ActionTypes.POST_REGISTER_STUDENT_FAILURE,  // Dispatch failure action if there's an error
         payload: err.response.data.message,
       })
     });
-
 };
 
+// Action to handle supervisor registration form submission
 export const postFormRegisterSupervisor = (data) => (dispatch) => {
   axios
-    .post(`/register-supervisor`, data)
-    .then((res) => {
-      return res.data;
+    .post(`/register-supervisor`, data)  // Send POST request to register supervisor endpoint
+    .then((res) => {  
+      return res.data;   // Return the response data
     })
     .then((result) =>
       dispatch({
-        type: ActionTypes.POST_REGISTER_SUPERVISOR_SUCCESS,
+        type: ActionTypes.POST_REGISTER_SUPERVISOR_SUCCESS,  // Dispatch success action with the result
         payload: result,
       })
     )
     .then(() => {
-      window.location.reload();
+      window.location.reload(); // Reload the page after successful registration
     })
     .catch((err) => {
       dispatch({
-        type: ActionTypes.POST_REGISTER_SUPERVISOR_FAILURE,
+        type: ActionTypes.POST_REGISTER_SUPERVISOR_FAILURE, // Dispatch failure action if there's an error
         payload: err.response.data.message,
       })
     });
-
 };
 
-
+// Action to handle user logout
 export const logoutUser = () => {
   return {
-    type: "LOGOUT_USER",
+    type: "LOGOUT_USER",  // Dispatch logout action
   };
 };
 
+// Action to fetch list of faculties
 export const fetchFaculties = () => (dispatch) => {
-  axios.get(`http://localhost:8080/faculties`)
+  axios.get(`http://localhost:8080/faculties`)  // Send GET request to fetch faculties data
     .then(res => {
-      // console.log(res.data);
-      return res.data
+      return res.data // Return the response data
     })
     .then((result) =>
       dispatch({
-        type: ActionTypes.FETCH_FACULTIES,
+        type: ActionTypes.FETCH_FACULTIES,  // Dispatch action with the faculties data
         payload: result,
       })).catch(err => {
-        console.log(err);
+        console.log(err); // Log any errors that occur during the request
       })
 }
 
+// Action to trigger page reload
 export const ReloadPage = () => {
   return{
-    type: ActionTypes.RELOAD_PAGE,
+    type: ActionTypes.RELOAD_PAGE,  // Dispatch reload action
   };
 }
-
--- pr-frontend/src/redux/apiReducers.js
+++ pr-frontend/src/redux/apiReducers.js
@@ -1,89 +1,86 @@
-import * as ActionTypes from './ActionTypes';
+import * as ActionTypes from './ActionTypes';    // Import action types
 
-
+// Initial state of the Redux store
 const initialState = {
-    isLoggedIn: false,
-    userLogin: {},
-    isRegistered: false,
-    faculties: [],
-    err_login: false,
-    err_register_comp: "",
-    err_register_stud: "",
-
+    isLoggedIn: false, // Indicates if the user is logged in
+    userLogin: {}, // Holds user login data
+    isRegistered: false, // Indicates if the user is successfully registered
+    faculties: [], // List of faculties fetched from the server
+    err_login: false, // Holds login error status
+    err_register_comp: "", // Holds company registration error message
+    err_register_stud: "", // Holds student registration error message
 }
 
+// Reducer function to handle different actions and update the state
 export default function reducer(state = initialState, action) {
     switch (action.type) {
-        case ActionTypes.POST_LOGIN_SUCCESS:
+        case ActionTypes.POST_LOGIN_SUCCESS:    // Action when login is successful
             return {
                 ...state,
-                userLogin: action.payload,
-                isLoggedIn: true,
-                err_login: false
+                userLogin: action.payload, // Store user login data
+                isLoggedIn: true, // Mark user as logged in
+                err_login: false // Clear login error
             };
-        case ActionTypes.POST_LOGIN_FAILURE:
+        case ActionTypes.POST_LOGIN_FAILURE:     // Action when login fails
             return {
                 ...state,
-                userLogin: action.payload,
-                err_login: true
+                userLogin: action.payload,  // Store the error payload
+                err_login: true // Set login error flag
             }
-        case ActionTypes.POST_REGISTER_SUCCESS:
+        case ActionTypes.POST_REGISTER_SUCCESS: // Action when company registration is successful
             return {
                 ...state,
-                isRegistered: true,
-                err_register_comp: ""
+                isRegistered: true, // Mark user as registered
+                err_register_comp: ""   // Clear company registration error
             }
-        case ActionTypes.POST_REGISTER_STUDENT_SUCCESS:
+        case ActionTypes.POST_REGISTER_STUDENT_SUCCESS: // Action when student registration is successful
             return {
                 ...state,
-                isRegistered: true,
-                err_register_stud: ""
+                isRegistered: true, // Mark user as registered
+                err_register_stud: ""   // Clear student registration error
 
             }
-        case ActionTypes.POST_REGISTER_FAILURE:
+        case ActionTypes.POST_REGISTER_FAILURE:  // Action when company registration fails
             return {
                 ...state,
-                err_register_comp: action.payload
+                err_register_comp: action.payload   // Set company registration error message
             }
-        case ActionTypes.POST_REGISTER_STUDENT_FAILURE:
+        case ActionTypes.POST_REGISTER_STUDENT_FAILURE: // Action when student registration fails
             return {
                 ...state,
-                err_register_stud: action.payload
+                err_register_stud: action.payload    // Set student registration error message
             }
-        case ActionTypes.POST_REGISTER_SUPERVISOR_SUCCESS:
+        case ActionTypes.POST_REGISTER_SUPERVISOR_SUCCESS:  // Action when supervisor registration is successful
             return {
                 ...state,
-                isRegistered: true,
+                isRegistered: true, // Mark user as registered
 
             }
-        case ActionTypes.POST_REGISTER_SUPERVISOR_FAILURE:
+        case ActionTypes.POST_REGISTER_SUPERVISOR_FAILURE:  // Action when supervisor registration fails
             return {
                 ...state,
+                // No specific changes on failure for supervisor registration
             }
 
-        case ActionTypes.LOGOUT_USER:
+        case ActionTypes.LOGOUT_USER:   // Action when user logs out
             return {
                 ...state,
-                userLogin: null,
-                isLoggedIn: false
+                userLogin: null, // Clear user login data
+                isLoggedIn: false // Mark user as logged out
             }
-        case ActionTypes.FETCH_FACULTIES:
+        case ActionTypes.FETCH_FACULTIES:    // Action to set fetched list of faculties
             return {
                 ...state,
-                faculties: action.payload
+                faculties: action.payload   // Update faculties data
             }
-        case ActionTypes.RELOAD_PAGE:
+        case ActionTypes.RELOAD_PAGE:   // Action to reset errors when page reloads
             return {
                 ...state,
-                err_register_comp: "",
-                err_register_stud: "",
-                err_login: false
+                err_register_comp: "", // Clear company registration error
+                err_register_stud: "", // Clear student registration error
+                err_login: false // Clear login error
             }
-
-        default:
+        default:     // Return current state if no matching action is found
             return state;
     }
 }
-
-
-
