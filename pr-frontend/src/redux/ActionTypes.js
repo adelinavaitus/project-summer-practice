fd08db8a6ea48fb1f1c6821d0cf3efd82be13148
Adelina Vaitus <adelinavaitus@gmail.com>
2025-02-11 10:07:56 +0200

    Added missing ActionTypes file

---
+++ pr-frontend/src/redux/ActionTypes.js
@@ -0,0 +1,24 @@
+// Action types for login
+export const POST_LOGIN_SUCCESS = 'POST_LOGIN_SUCCESS'; // Dispatched when login is successful
+export const POST_LOGIN_FAILURE = 'POST_LOGIN_FAILURE'; // Dispatched when login fails
+
+// Action types for company registration
+export const POST_REGISTER_SUCCESS = 'POST_REGISTER_SUCCESS'; // Dispatched when company registration is successful
+export const POST_REGISTER_FAILURE = 'POST_REGISTER_FAILURE'; // Dispatched when company registration fails
+
+// Action types for student registration
+export const POST_REGISTER_STUDENT_SUCCESS = 'POST_REGISTER_STUDENT_SUCCESS'; // Dispatched when student registration is successful
+export const POST_REGISTER_STUDENT_FAILURE = 'POST_REGISTER_STUDENT_FAILURE'; // Dispatched when student registration fails
+
+// Action types for supervisor registration
+export const POST_REGISTER_SUPERVISOR_SUCCESS = 'POST_REGISTER_SUPERVISOR_SUCCESS'; // Dispatched when supervisor registration is successful
+export const POST_REGISTER_SUPERVISOR_FAILURE = 'POST_REGISTER_SUPERVISOR_FAILURE'; // Dispatched when supervisor registration fails
+
+// Action type for user logout
+export const LOGOUT_USER = 'LOGOUT_USER'; // Dispatched to log out the user
+
+// Action type for fetching faculties
+export const FETCH_FACULTIES = 'FETCH_FACULTIES'; // Dispatched to fetch the list of faculties
+
+// Action type for reloading the page
+export const RELOAD_PAGE = 'RELOAD_PAGE'; // Dispatched to trigger a page reload
